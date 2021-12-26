40b84cfc7f799d2546d7204ab7f3134c1370a8de
Algirdas Č <18305296+algirdasc@users.noreply.github.com>
2021-12-26 22:32:45 +0200

    Update switch.py

--- custom_components/floureon/switch.py
+++ custom_components/floureon/switch.py
@@ -20,7 +20,7 @@ _LOGGER = logging.getLogger(__name__)
 
 import voluptuous as vol
 
-from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
+from homeassistant.components.switch import SwitchEntity, PLATFORM_SCHEMA
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.const import (
     CONF_NAME,
@@ -63,7 +63,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities([FloureonSwitch(hass, config)])
 
 
-class FloureonSwitch(SwitchDevice, RestoreEntity):
+class FloureonSwitch(SwitchEntity, RestoreEntity):
 
     def __init__(self, hass, config):
         if config.get(CONF_MAC) is not None:
